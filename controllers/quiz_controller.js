0ae9a2926534d2630785efa32dcf8aa8ced8a857
Mario Durán <mduran16@outlook.com>
2015-07-15 21:32:18 +0200

    P12: Validación de entradas (hay que desinstalar sequelize e instalar la version 2.0.0

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -9,7 +9,7 @@ exports.load = function(req, res, next, quizId) {
             next();
          } else {
             next(new Error('No existe quizId = ' + quizId));
-         }
+         };
       }
    ).catch(
       function(error) {
@@ -40,7 +40,7 @@ exports.index = function(req, res) {
    }
    models.Quiz.findAll(options).then(
       function(quizes) {
-         res.render('quizes/index', {quizes: quizes})
+         res.render('quizes/index', {quizes: quizes, errors: []})
       }
    ).catch(
       function(error) {
@@ -51,16 +51,16 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-   res.render('quizes/show', {quiz: req.quiz});
+   res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-   var resultado = 'Incorrecto'
-   if (req.query.respuesta === req.quiz.respuesta) {
+   var resultado = 'Incorrecto';
+   if (req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()) {
       resultado = 'Correcto';
-   }
-   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+   };
+   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -70,18 +70,34 @@ exports.new = function(req, res) {
    var quiz = models.Quiz.build(
       {pregunta: "Pregunta", respuesta: "Respuesta"}
    );
-   res.render('quizes/new', {quiz: quiz});
+   res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
-   var quiz = models.Quiz.build(req.body.quiz);
+   var quiz = models.Quiz.build( req.body.quiz );
 
-   // Guardar en BD sólo los campos pregunta y respuesta
-   quiz.save({fields: ["pregunta", "respuesta"]}).then(
-      function() {
-         // Redireccionar a la lista de preguntas
-         res.redirect('/quizes');
+   /* Para que funciona el 'then' después del 'validate', hay que desinstalar
+      la versión de sequelize que indican en el curso e instalar la versión 2,
+      de la forma siguiente:
+         npm uninstall sequelize
+         npm install -save sequelize@2.0.0
+   */
+   quiz.validate().then(
+      function(err) {
+         if (err) {
+            res.render('quizes/new', {quiz: quiz, errors: err.errors});
+         } else {
+
+            // Guardar en BD sólo los campos pregunta y respuesta
+            quiz.save({fields: ["pregunta", "respuesta"]}).then(
+               function() {
+
+               // Volver a la lista de Preguntas
+               res.redirect('/quizes')
+               }
+            );
+         }
       }
    );
 };
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController = require('../controllers/quiz_controller');
 
 // Página de entrada o Home Page
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
